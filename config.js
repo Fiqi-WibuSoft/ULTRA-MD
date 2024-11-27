a4bb981b38258d4b095649207fd3f6f9f905feb3
hihihi <95555507+Fiqi-WibuSoft@users.noreply.github.com>
2024-11-27 13:58:38 +0700

    config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '6287822090456','6287816687002';
+const defaultOwner = '6287822090456';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
